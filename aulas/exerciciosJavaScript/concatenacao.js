d0ae27d82ae2715ade7098df15de47db3d1b5eb4
Carolina Lopes <carolina.b.lopes@hotmail.com>
2022-06-06 15:21:55 -0300

    Subindo Exercicios Propostos em Aula

---
+++ aulas/exerciciosJavaScript/concatenacao.js
@@ -0,0 +1,11 @@
+const primeiroNome = "Carolina";
+const ultimoNome = "Lopes";
+const idade = 28;
+
+
+console.log("Meu nome é " + primeiroNome + " " + ultimoNome + " e tenho " + idade + " anos.");
+
+
+console.log(
+    `Meu nome é ${primeiroNome} ${ultimoNome} e tenho ${idade} anos.`
+    );
---
+++ aulas/exerciciosJavaScript/lacoCondicao.js
@@ -0,0 +1,34 @@
+ const numeroA = 50;
+const numeroB = 50;
+let soma = numeroA + numeroB;
+
+if (soma > 100) {
+    console.log("A soma é maior que 100");
+} else if (soma === 100) {
+    console.log("BINGO! Numero igual a 100");
+} else {
+    console.log("É um número menor que 100");
+}
+
+
+const idade = 12;
+
+if (idade >= 5 && idade <= 7) {
+    console.log("Infantil A");
+} else if (idade >= 8 && idade <= 11) {
+    console.log("Infantil B");
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+<script type="text/javascript" src="variaveis.js"></script>
+*/
