70a07b48ddedfc9711f486107a3563e720c10af5
Carolina Lopes <carolina.b.lopes@hotmail.com>
2022-06-19 21:13:28 -0300

    Subindo entregável 02 de JavaScript

---
+++ aulas/entregaveisJavaScript/ex02.js
@@ -0,0 +1,12 @@
+/* 
+Crie um algoritmo que calcule a velocidade média de um veículo e retorne o resultado no console.
+Dica do sucesso: A velocidade média é calculada dividindo a distancia percorrida pelo tempo gasto.
+ */
+
+
+const distanciaKm = 100;
+const tempoHoras = 5;
+
+
+const velocidadeMedia = distanciaKm / tempoHoras;
+console.log(`A velocidade média percorrida por um veículo é de ${velocidadeMedia}Km/h.`);
