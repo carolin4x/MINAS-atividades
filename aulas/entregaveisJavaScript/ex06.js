6459484c45bd8273f525e0768d8bec502095dce5
Carolina Lopes <carolina.b.lopes@hotmail.com>
2022-06-19 21:34:27 -0300

    Subindo entregável 06 de JavaScript

--- aulas/entregaveisJavaScript/ex06.js
+++ aulas/entregaveisJavaScript/ex06.js
@@ -1,3 +1,14 @@
+/*
+Crie uma função que receba como parâmetro o nome da pessoa e a idade, 
+
+caso a pessoa seja maior de 18 anos a função deve retornar "nomeDaPessoa é de maior e tem x anos", 
+caso seja de menor "nomeDaPessoa é de maior e tem x anos".
+
+
+Em seguida chame essa função quatro vezes, passando 3 diferentes dados.
+*/
+
+
 let nome = prompt('Informe seu nome: ')
 let idade = parseFloat(prompt('Informe sua idade: '))
 
